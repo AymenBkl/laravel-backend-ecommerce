4eac58af6efc852b13d901cecd34698611d1f5c3
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2020-10-13 15:56:20 +0100

    ready push

---
+++ storage/framework/views/a77058552dc57e3bc014099e1e10fdd9ed5cb4dc.php
@@ -0,0 +1,7 @@
+<!-- Banners Content -->
+<section class="banners-content">
+
+  <?php  echo $final_theme['banner']; ?>
+
+</section>
+<?php /**PATH K:\Freelancer\LAravel Ionic Fish\LaravelWebsite&AdminCode\resources\views/web/product-sections/banner_section.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c14ab554b300358429229d3257812b5ae5450bb3.php
@@ -0,0 +1,5 @@
+<script>
+
+
+</script>
+<?php /**PATH K:\Freelancer\LAravel Ionic Fish\LaravelWebsite&AdminCode\resources\views/web/common/scripts/Like.blade.php ENDPATH**/ ?>
